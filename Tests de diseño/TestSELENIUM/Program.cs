a416ec1ed8fb17a7008b32a0a2b0fce85f53cc1b
Ele984 <90991229+Ele984@users.noreply.github.com>
2021-09-18 18:03:01 -0600

    Add files via upload

---
+++ Tests de diseño/TestSELENIUM/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace TestSELENIUM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creamos el objeto para que maneje el navegador
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("https://www.youtube.com/");
+            driver.Manage().Window.Maximize(); //**
+
+
+        }
+    }
+}
